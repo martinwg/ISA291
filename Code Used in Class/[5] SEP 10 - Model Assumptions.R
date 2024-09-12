8027879c575eb5d53f2bbfb8333a56d9d55d4213
martinwg <martinwg@miamioh.edu>
2024-09-12 10:04:24 -0400

    Add files via upload

--- Code Used in Class/[5] SEP 10 - Model Assumptions.R
+++ Code Used in Class/[5] SEP 10 - Model Assumptions.R
@@ -1,173 +1,90 @@
-## The least-squares method
-## Matrix notation
+## COLLEGE SCORECARD EXAMPLE
+#### Goal: Assumptions of linear regression
 
-df = read.csv('https://raw.githubusercontent.com/martinwg/ISA291/main/data/Analyst_SalaryData.csv',
+df = read.csv('https://raw.githubusercontent.com/martinwg/ISA291/main/data/college_scorecard.csv',
               stringsAsFactors = T)
 
-## Fit a regression
-## y = Salary
-## x = YearsExperience
+## print obs
+head(df)
 
-reg1 = lm(Salary ~ YearsExperience, data = df)
-summary(reg1) ## all statistics
-reg1
+## Create a new data set (overwrite df)
+## selecting COSTT4_A and MD_EARN_WNE_P10
+df = df[ ,c("COSTT4_A", "MD_EARN_WNE_P10")]
+head(df)
 
-## plot the regression (1 single predictor)
-plot(df$YearsExperience, df$Salary)
+## Note: NA means missing
+## let's omit them
+## imputation will be used later in the course
+df = na.omit(df)
+head(df)
+
+## Plot (scatter) of the relationship
+## expecting +
+plot(df$COSTT4_A, df$MD_EARN_WNE_P10)
+
+## REGRESSION
+options(scipen = 999)
+reg1 = lm(MD_EARN_WNE_P10 ~ COSTT4_A, data = df)
+summary(reg1)    ## estimates
+anova(reg1)      ## SSE
+
+
+## Intercept: We ESTIMATE the earnings to be $30658.18
+## for someone whose college was free of charge
+## extrapolation: means predicting (explaining) data
+## outside the range of values seen
+
+plot(df$COSTT4_A, df$MD_EARN_WNE_P10)
 abline(reg1, col = "red", lwd = 3)
 
-## residuals
-reg1$residuals
+## Slope: As the cost of college increases by $1,
+## we ESTIMATE the earnings to increase by about 51 cents
 
-## anova table shows SSE
-options(scipen = 999)
-anova(reg1)
+## What is SSE?
+## SSE = 439680330339 (minimized, the lower the better)
 
-## What if we have two predictors
-reg2 = lm(Salary ~ YearsExperience + Age, data = df)
-summary(reg2)
+## ASSUMPTIONS
+## ASSUMPTION # 1) MEAN(ERROR TERM) = 0
+## check on the residuals
+mean(reg1$residuals)
+## new data set - calculate e = suppose mean(e) = + or -
+## means estimates are BIASED (y-int or slope)
 
-## lm(Salary ~ YearsExperience, data)
-y = df$Salary ## sets this as a vector
-## predictors are put in a matrix
-## [1 YearsExperience]
-## the model matrix (design matrix)
-model.matrix(reg1)
+## ASSUMPTION # 2) VAR(ERROR TERM) = constant
+## check on the residulas
+var(reg1$residuals) ## check that number (this is BIASED)
 
-## Matrices 
-## 3x3 dimension
-A = matrix(c(3.5, 1, 80, 4.0,2, 84, 3.2, 3, 120), nrow = 3, byrow = T)
-A
+## MSE is the estimate of the var(E)
+## MSE = SSE / (n - (k+1))
+## MSE =  142799718 (VARIANCE)
 
-## dim obtains the dimension
-dim(A)
+## square root of MSE (RMSE)
+## RMSE is called also residual standard error
+## RMSE 11950 (error is estimated at $11,950)
 
-## What is the dimension of X matrix (x = YearsExperience, y = Salary)?
-## n X (p+1)
-## answer: 30 x 2
-dim(model.matrix(reg1))
+## The sd is 11950, that means that about 99% of the 
+## predictions should be within 3*sd away 
 
+## IF THIS DOES NOT HOLD
+## Larger standard errors than what was estimated
+## p-values might not be completely true
+## with NOT TOO MANY obs, the issues are the worst
 
+## to check assumption
+## 1) look at scatter plot (image RMSE be constant)
+## 2) plot the residuals (y) vs predicted (x) (better)
+plot(reg1$fitted.values, reg1$residuals)
 
-## What is the dimension of X matrix 
-## (x1 = YearsExperience, x2 = Age, y = Salary)?
-## n X (p+1)
-## answer: 30 x 3
-dim(model.matrix(reg2))
+## We can do this automatically
+plot(reg1) ## gives you some graphs include e vs yhat
 
-## y vector
-## dim nx1 
-## What is the dimension of y vector
-## answer:30x1
+## if violated: estimates are unbiased (y-int, slope)
+## the model is NOT necessarily wrong (specially with large n)
 
-## Transpose of a matrix
-## swaps rows and cols
-A
-## to get the transpose of A
-t(A)
-
-## What is the transpose of the X matrix
-## x = YearsExperience
-## y = Salary
-model.matrix(reg1)
-
-## transpose
-t(model.matrix(reg1))
-
-
-## matrix B
-B = matrix(c(0, 0, 1, 0,0,1, 0,0, 1), ncol = 3, byrow = T)
-B
-
-## Add matrices A and B
-A + B
-
-## Subtract B from A
-A - B
-
-## matrix C (2x3)
-C = matrix(c(0, 0, 1, 0,0,1), ncol = 3, byrow = T)
-C
-
-## Add A + C
-A+C
-## don't match (not conformable)
-
-## Scalar multiplication
-3*A
-
-## for matrix multiplication
-## cols of A MUST match rows of B
-dim(A)  ## 3 x 3
-dim(B)  ## 3 x 3
-
-A %*% B  ## conformable (match)
-
-
-## example 2
-dim(A) ## 3 x 3
-dim(C) ## 2 x 3
-
-A %*% C
-
-## example 3
-dim(A)    ## 3 x 3
-dim(t(C)) ## 3 x 2
-
-A %*% t(C)
-
-## matrices are DEPENDENT if columns are linear combinations of each
-## other
-## determinant of the matrix is zero for DEPENDENT
-det(A) ## if det is NOT zero it is a good matrix
-
-## matrix B
-det(B) ## SINGULAR (det is zero): bad matrix
-
-## inverse
-## equivalent to 1/number 
-## for matrices we divide by the determinant (if det is zero)
-solve(A) ## gets inverse
-
-## inverse for B not going to work (det is zero)
-solve(B)
-
-## matrix has to be square (rows = cols)
-solve(C)
-
-## the regression estimates are obtain ALL at the same time
-## we use a single formulat
-## estimates  = (X'X)^-1 (X'y)
-
-## x = YearsExperience
-## y = Salary
-X = model.matrix(reg1)
-y = df$Salary
-
-## betahat
-betahat = solve(t(X) %*% X)  %*%  (t(X) %*% y)
-betahat
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## ASSUMPTION # 3. Distribution of ERROR TERM ~ N(0, SIGMA^2)
+## check on residuals  e ~ N(0, 11950)
+hist(reg1$residuals, col = "red", breaks = 20)
+plot(reg1)
 
 
 
