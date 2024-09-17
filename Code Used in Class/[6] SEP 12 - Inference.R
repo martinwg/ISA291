af5805b40c555f67919a7dea0a69d40ef243a1e2
martinwg <martinwg@miamioh.edu>
2024-09-17 11:25:03 -0400

    Add files via upload

--- Code Used in Class/[6] SEP 12 - Inference.R
+++ Code Used in Class/[6] SEP 12 - Inference.R
@@ -74,6 +74,9 @@ anova(reg1)
 
 ## t = b1/s_b1 =  0.50979/0.01196 = 42.62
 ## estimate  is 42.62 std errors away from zero
+## t ~ t-dist(n - (k+1))
+## t ~ t-dist(3081 - (1+1))
+## t - t-dist(3079)
 
 ## p-value
 ## prob of getting a 42.62 t-statistic
@@ -105,9 +108,50 @@ cor(df$COSTT4_A, df$MD_EARN_WNE_P10)
 ## R2 = SSR / SST (ration of explained / total)
 SST = 439680330339 + 259449312939
 
+## Assumptions
+## 1) mean(E) = 0
+## 2) Var(E) = sigma^2 (estimated by MSE)
+## 3) E ~ N(0, sigma^2)
+## 4) E are independent
+
+plot(reg1)
+
+## plot 1 (residuals vs predicted) assumption 2
+## plot 2 (QQ-plot) assumption 3
+
+## supplement these plots with other plots or tests
+hist(reg1$residuals) ## assumption 3
+shapiro.test(reg1$residuals)
+## formal test on assumption 3 (normality)
+## H0: residuals are NORMAL
+## HA: residuals are NOT NORMAL
+## P-VALUE: probability that you get these residuals
+## if H0 is true
+## < 0.0000000000000002
+## evidence against normality
 
 
+## Point estimates
+## estimated values, predictions
+## to estimate given the reg function
+
+## Cost of college = $10,000
+## What is the estimated earnings?
+## mean of earnings for ALL students with cost $10,000
+
+30658.18159 + 0.50979 * 10000
+predict(reg1, df)  ## prediction for ALL OBS
+predict(reg1, data.frame(COSTT4_A = 10000))  ## 1 obs
 
 
+## Confidence interval on the mean
+## For ALL students whose cost of college is $10,000
+## We are 95% confident the mean earnings will be between 35130.56  and 36381.54
+predict(reg1, data.frame(COSTT4_A = 10000), interval = "confidence")
+
+## prediction interval on an individual
+## For an individual student whose cost of college is $10,000
+## We are 95% confident earnings will be between 12317.15 and 59194.95
+predict(reg1, data.frame(COSTT4_A = 10000), interval = "prediction")
 
 
